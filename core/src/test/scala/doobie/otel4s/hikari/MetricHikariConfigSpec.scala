166496e50703e3bfc1ea6a3bdb38d4fb7a2c7deb
Alix <alix.baggio@outlook.fr>
2025-05-01 16:50:40 +0200

    Add HikariTelemetryTransactor (metric+tracing)

---
+++ core/src/test/scala/doobie/otel4s/hikari/MetricHikariConfigSpec.scala
@@ -0,0 +1,19 @@
+package doobie.otel4s.hikari
+
+import com.zaxxer.hikari.HikariConfig
+import io.opentelemetry.api.OpenTelemetry
+import munit.FunSuite
+
+class MetricHikariConfigSpec extends FunSuite {
+
+  test("should wrap with an open telemetry metrics tracker") {
+    val config  = MetricHikariConfig.fromConfig(OpenTelemetry.noop(), new HikariConfig())
+    val metrics = config.getMetricsTrackerFactory
+    assertEquals(
+      metrics.getClass.getName,
+      "io.opentelemetry.instrumentation.hikaricp.v3_0.OpenTelemetryMetricsTrackerFactory"
+    )
+
+  }
+
+}
